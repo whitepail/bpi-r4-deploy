f4c92ab726d3df729adb3c09ed2f7d63e885c89d
Alexander Belozerov <alexander.belozerov@gmail.com>
2026-09-14 08:53:31 +0000

    Added 256mb nand to kernel dtsi

--- builder-wifimgr-universal.sh
+++ builder-wifimgr-universal.sh
@@ -41,6 +41,7 @@ bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x
 
 
 \cp -r ../my_files/453-w-add-bpi-r4-nvme-dtso.patch target/linux/mediatek/patches-6.12/
+\cp -r ../my_files/191-arm64-dts-mediatek-mt7988a-bpi-r4-256nand.patch target/linux/mediatek/patches-6.12/
 \cp -r ../my_files/450-w-nand-mmc-add-bpi-r4.patch package/boot/uboot-mediatek/patches/450-add-bpi-r4.patch
 \cp -r ../my_files/451-w-add-bpi-r4-nvme.patch package/boot/uboot-mediatek/patches/451-add-bpi-r4-nvme.patch
 \cp ../my_files/452-w-add-bpi-r4-nvme-rfb.patch package/boot/uboot-mediatek/patches/452-add-bpi-r4-nvme-rfb.patch
